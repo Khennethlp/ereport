29f9bc1a0fd89d80d94c8de96f441b8353c98838
khenneth <khennethlp@gmail.com>
2024-09-19 11:03:41 +0800

    removed animation shake

--- pages/admin/plugins/preloader.php
+++ pages/admin/plugins/preloader.php
@@ -1,6 +1,6 @@
   <!-- Preloader -->
   <div id="preloader" class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="../../dist/img/hourglass.gif" alt="logo" height="60" width="60">
+    <img class="" src="../../dist/img/hourglass.gif" alt="logo" height="60" width="60">
     <noscript>
       <br>
       <span>We are facing <strong>Script</strong> issues. Kindly enable <strong>JavaScript</strong>!!!</span>
--- pages/checker/plugins/navbar.php
+++ pages/checker/plugins/navbar.php
@@ -104,7 +104,7 @@ if (!isset($_SESSION['username'])) {
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="../../dist/img/hourglass.gif" alt="logo" height="60" width="60">
+      <img class="" src="../../dist/img/hourglass.gif" alt="logo" height="60" width="60">
       <noscript>
         <br>
         <span>We are facing <strong>Script</strong> issues. Kindly enable <strong>JavaScript</strong>!!!</span>
